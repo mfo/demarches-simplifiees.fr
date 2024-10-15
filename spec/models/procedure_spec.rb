1ddc1483bbb148932aa1b1a640f65cdedd138254
Christophe Robillard <christophe.robillard@mail.numerique.gouv.fr>
2024-10-15 10:51:16 +0200

    use with_header_section

--- spec/models/procedure_spec.rb
+++ spec/models/procedure_spec.rb
@@ -1906,6 +1906,10 @@ describe Procedure do
         expect(procedure.all_revisions_types_de_champ.size).to eq 1
       end
 
+      it 'returns also section type de champ' do
+        expect(procedure.all_revisions_types_de_champ(with_header_section: true).size).to eq 2
+      end
+
       it "returns types de champ on draft revision" do
         procedure.draft_revision.add_type_de_champ(type_champ: :text, libelle: 'onemorechamp')
         expect(procedure.reload.all_revisions_types_de_champ.size).to eq 2
@@ -1918,6 +1922,11 @@ describe Procedure do
       it 'returns one type de champ' do
         expect(procedure.all_revisions_types_de_champ.size).to eq 1
       end
+
+      it 'returns also section type de champ' do
+        expect(procedure.all_revisions_types_de_champ(with_header_section: true).size).to eq 2
+      end
+
 
       it "doesn't return types de champ on draft revision" do
         procedure.draft_revision.add_type_de_champ(type_champ: :text, libelle: 'onemorechamp')
