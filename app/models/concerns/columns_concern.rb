fe2159db4024ba2a5157d191e3ed4ced33056d97
Christophe Robillard <christophe.robillard@mail.numerique.gouv.fr>
2024-10-17 17:28:11 +0200

    wip export

--- app/models/concerns/columns_concern.rb
+++ app/models/concerns/columns_concern.rb
@@ -12,7 +12,7 @@ module ColumnsConcern
       column = columns.find { _1.h_id == h_id } if h_id.present?
       column = columns.find { _1.label == label } if label.present?
 
-      raise ActiveRecord::RecordNotFound if column.nil?
+      raise ActiveRecord::RecordNotFound.new(label) if column.nil?
 
       column
     end
