c8db53daa5b90f107824061d324fe645cca24d89
Kara Diaby <kdiaby.pro@gmail.com>
2026-01-19 15:51:14 +0100

    Add dossierAnnulerDemandeCorrection mutation and cancelCorrection parameter

--- app/graphql/mutations/dossier_supprimer_message.rb
+++ app/graphql/mutations/dossier_supprimer_message.rb
@@ -6,18 +6,28 @@ module Mutations
 
     argument :message_id, ID, required: true, loads: Types::MessageType
     argument :instructeur_id, ID, required: true, loads: Types::ProfileType
+    argument :cancel_correction,
+      GraphQL::Types::Boolean,
+      required: false,
+      default_value: true,
+      description: "Annule automatiquement la demande de correction avant de supprimer le message.",
+      deprecation_reason: "Utilisez la mutation `dossierAnnulerDemandeCorrection` avant de supprimer un message avec correction. Ce paramètre sera supprimé dans une future version."
 
     field :message, Types::MessageType, null: true
     field :errors, [Types::ValidationErrorType], null: true
 
-    def resolve(message:, **args)
+    def resolve(message:, cancel_correction:, **args)
+      if cancel_correction && message.dossier_correction&.pending?
+        message.cancel_correction!
+      end
+
       message.soft_delete!
 
       { message: }
     end
 
-    def authorized?(message:, instructeur:, **args)
-      if !message.soft_deletable?(instructeur)
+    def authorized?(message:, instructeur:, cancel_correction:, **args)
+      if !message.soft_deletable?(instructeur, cancel_correction:)
         return false, { errors: ["Le message ne peut pas être supprimé"] }
       end
       dossier_authorized_for?(message.dossier, instructeur)
