0ffbe620d25e706d5a6aeeef02483d720a7f105e
Mathieu Magnin <mathieu@magnin.sh>
2026-01-27 16:54:43 +0100

    fix(repetition): when row has errors expand it

--- app/components/editable_champ/repetition_component.rb
+++ app/components/editable_champ/repetition_component.rb
@@ -20,4 +20,22 @@ class EditableChamp::RepetitionComponent < EditableChamp::EditableChampBaseCompo
   def aria_controls
     @champ.row_ids.map { |row_id| "#{@champ.html_id}-#{row_id}-accordion-accordion-content" }.join(' ')
   end
+
+  def expand_row?(row_id, rows_count)
+    rows_count == 1 || row_has_errors?(row_id)
+  end
+
+  def row_has_errors?(row_id)
+    dossier = @champ.dossier
+    return false if dossier.errors.empty?
+
+    children_types = dossier.revision.children_of(@champ.type_de_champ)
+    public_ids = children_types.map { |tdc| tdc.public_id(row_id) }.to_set
+
+    public_ids.intersect?(errors_public_ids)
+  end
+
+  def errors_public_ids
+    @errors_public_ids ||= @champ.dossier.errors.map { |error| error.inner_error.base.public_id if error.is_a?(ActiveModel::NestedError) && error.inner_error.base.respond_to?(:public_id) }.compact.to_set
+  end
 end
