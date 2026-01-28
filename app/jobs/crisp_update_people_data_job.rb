87bb10a1b07819f495fe1c960ef02c125ed15c39
Colin Darie <colin@darie.eu>
2026-01-28 12:31:30 +0100

    refactor(crisp): extract update_people_data into dedicated method

--- app/jobs/crisp_update_people_data_job.rb
+++ app/jobs/crisp_update_people_data_job.rb
@@ -11,19 +11,7 @@ class CrispUpdatePeopleDataJob < ApplicationJob
     email ||= fetch_email_from_session(session_id)
     user = User.find_by!(email:)
 
-    user_data = Crisp::UserDataBuilder.new(user).build_data
-
-    result = Crisp::APIService.new.update_people_data(
-      email: user.email,
-      body: { data: user_data }
-    )
-
-    case result
-    in Success
-      # NOOP
-    in Failure(reason:)
-      fail reason
-    end
+    update_people_data(user)
   end
 
   private
@@ -33,6 +21,18 @@ class CrispUpdatePeopleDataJob < ApplicationJob
     case result
     in Success(data: { email: })
       email
+    in Failure(reason:)
+      fail reason
+    end
+  end
+
+  def update_people_data(user)
+    user_data = Crisp::UserDataBuilder.new(user).build_data
+    result = Crisp::APIService.new.update_people_data(email: user.email, body: { data: user_data })
+
+    case result
+    in Success
+    # NOOP
     in Failure(reason:)
       fail reason
     end
