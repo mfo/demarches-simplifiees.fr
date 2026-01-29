794e35a03adbe93f30dd920dba8c677538ee7140
Mathieu Magnin <mathieu@magnin.sh>
2026-01-29 18:59:37 +0100

    fix(test): split shared examples by error class

--- spec/support/shared_examples_for_jobs.rb
+++ spec/support/shared_examples_for_jobs.rb
@@ -7,12 +7,6 @@ RSpec.shared_examples 'a job retrying transient errors' do |job_class = describe
     end
   end if !defined?(ExconErrorJob)
 
-  StandardErrorJob = Class.new(job_class) do
-    def perform
-      raise StandardError
-    end
-  end if !defined?(StandardErrorJob)
-
   context 'when a transient network error is raised' do
     it 'makes 5 attempts before raising the exception up' do
       assert_performed_jobs 5 do
@@ -20,6 +14,14 @@ RSpec.shared_examples 'a job retrying transient errors' do |job_class = describe
       end
     end
   end
+end
+
+RSpec.shared_examples 'a job retrying standard errors' do |job_class = described_class|
+  StandardErrorJob = Class.new(job_class) do
+    def perform
+      raise StandardError
+    end
+  end if !defined?(StandardErrorJob)
 
   context 'when another type of error is raised' do
     it 'makes 25 attempts before raising the exception up (default strategy)' do
