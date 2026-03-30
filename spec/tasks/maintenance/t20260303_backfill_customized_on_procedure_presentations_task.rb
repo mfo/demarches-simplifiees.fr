f4c6bb4f18bf0f6f25d929400ab9b1e03a1a5694
Lisa Durand <lisa.c.durand@gmail.com>
2026-03-30 18:55:20 +0200

    fix backfill procedure_presentation displayed_columns customization task

--- spec/tasks/maintenance/t20260303_backfill_customized_on_procedure_presentations_task.rb
+++ spec/tasks/maintenance/t20260303_backfill_customized_on_procedure_presentations_task.rb
@@ -4,38 +4,58 @@ module Maintenance
   RSpec.describe T20260303BackfillCustomizedOnProcedurePresentationsTask do
     describe "#process" do
       let(:procedure) { create(:procedure, :published) }
-      let(:instructeur) { create(:instructeur) }
+      let(:task) { described_class.new }
 
-      let!(:assign_to_default) { create(:assign_to, procedure: procedure, instructeur: instructeur) }
+      let(:instructeur_1) { create(:instructeur) }
+      let(:instructeur_2) { create(:instructeur) }
 
-      let!(:assign_to_custom) { create(:assign_to, procedure: procedure, instructeur: create(:instructeur)) }
+      let!(:assign_to_1) { create(:assign_to, procedure: procedure, instructeur: instructeur_1) }
+      let!(:assign_to_2) { create(:assign_to, procedure: procedure, instructeur: instructeur_2) }
 
       let!(:presentation_default) do
         create(
           :procedure_presentation,
-          assign_to: assign_to_default,
+          assign_to: assign_to_1,
           displayed_columns: procedure.default_displayed_columns
         )
       end
 
-      let!(:custom_column) { procedure.find_column(label: "Date de dépôt") }
-
       let!(:presentation_custom) do
-        pres = create(:procedure_presentation, assign_to: assign_to_custom)
-        pres.update!(displayed_columns: [custom_column])
-        pres
+        procedure_presentation = create(:procedure_presentation, assign_to: assign_to_2)
+
+        custom_column = procedure.find_column(label: "Date de dépôt")
+
+        procedure_presentation.update!(displayed_columns: [custom_column])
+        procedure_presentation
       end
 
-      it "marks presentations with default columns as customized: false" do
-        task = described_class.new
-        task.collection.each { |p| task.process(p) }
+      it "sets customized to false when displayed columns match default" do
+        task.process(presentation_default)
+
         expect(presentation_default.reload.customized).to eq(false)
       end
 
-      it "marks presentations with custom columns as customized: true" do
-        task = described_class.new
-        task.collection.each { |p| task.process(p) }
+      it "sets customized to true when displayed columns differ from default" do
+        task.process(presentation_custom)
+
         expect(presentation_custom.reload.customized).to eq(true)
+      end
+
+      it "ignores order of h_id when comparing" do
+        column1 = instance_double("Column", h_id: "a")
+        column2 = instance_double("Column", h_id: "b")
+
+        allow(presentation_default)
+          .to receive(:displayed_columns)
+          .and_return([column2, column1])
+
+        allow(procedure)
+          .to receive(:default_displayed_columns)
+          .and_return([column1, column2])
+
+        task.process(presentation_default)
+
+        expect(presentation_default.reload.customized).to eq(false)
       end
     end
   end
