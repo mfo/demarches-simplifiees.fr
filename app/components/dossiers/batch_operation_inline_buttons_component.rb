ec6e5eb1c5d136371eed4bcc2bc637ad08e95e92
Lisa Durand <lisa.c.durand@gmail.com>
2026-04-14 10:25:04 +0200

    refactor: pass procedure to batch operation components

--- app/components/dossiers/batch_operation_inline_buttons_component.rb
+++ app/components/dossiers/batch_operation_inline_buttons_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Dossiers::BatchOperationInlineButtonsComponent < ApplicationComponent
-  attr_reader :opt, :icons, :form
+  attr_reader :opt, :icons, :form, :procedure
 
-  def initialize(opt:, icons:, form:)
+  def initialize(opt:, icons:, form:, procedure:)
     @opt = opt
     @icons = icons
     @form = form
+    @procedure = procedure
   end
 end
