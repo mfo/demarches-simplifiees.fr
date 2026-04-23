0ebfd09842f3965a3d8beb1b3d095126e8802469
Kara Diaby <kdiaby.pro@gmail.com>
2026-04-23 16:08:27 +0200

    feat(mes-dossiers): replace legacy filters with a desktop drawer filter panel

--- app/components/dossiers/user_procedure_filter_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Dossiers::UserProcedureFilterComponent < ApplicationComponent
-  include DossierHelper
-
-  def initialize(procedures_for_select:)
-    @procedures_for_select = procedures_for_select
-  end
-end
