7be161ca9af7cc6ec26d4a2833b7cfdb1ea44579
Kara Diaby <kdiaby.pro@gmail.com>
2026-04-23 15:08:56 +0200

    feat(mes-dossiers): map depose UI state to en_construction in DossierStateMapping

--- app/models/users/dossier_state_mapping.rb
+++ app/models/users/dossier_state_mapping.rb
@@ -2,12 +2,18 @@
 
 module Users
   module DossierStateMapping
-    UI_STATES = Dossier.states.values.freeze
+    UI_STATES = %w[brouillon depose en_instruction accepte refuse sans_suite].freeze
+    UI_TO_MODEL = { 'depose' => 'en_construction' }.freeze
 
     module_function
 
     def ui_states
       UI_STATES
+    end
+
+    def model_state_for(ui_state)
+      return nil unless UI_STATES.include?(ui_state)
+      UI_TO_MODEL.fetch(ui_state, ui_state)
     end
   end
 end
--- spec/models/users/dossier_state_mapping_spec.rb
+++ spec/models/users/dossier_state_mapping_spec.rb
@@ -3,9 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Users::DossierStateMapping do
+  describe '.model_state_for' do
+    it 'maps :depose to en_construction' do
+      expect(described_class.model_state_for('depose')).to eq('en_construction')
+    end
+
+    it 'returns the input for other known states' do
+      %w[brouillon en_instruction accepte refuse sans_suite].each do |state|
+        expect(described_class.model_state_for(state)).to eq(state)
+      end
+    end
+
+    it 'returns nil for unknown states' do
+      expect(described_class.model_state_for('unknown')).to be_nil
+    end
+  end
+
   describe '.ui_states' do
-    it 'exposes the 6 model states in display order' do
-      expect(described_class.ui_states).to eq(%w[brouillon en_construction en_instruction accepte refuse sans_suite])
+    it 'exposes the 6 UI states in display order' do
+      expect(described_class.ui_states).to eq(%w[brouillon depose en_instruction accepte refuse sans_suite])
     end
   end
 end
