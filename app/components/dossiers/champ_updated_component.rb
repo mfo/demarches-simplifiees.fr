17b7921f3fd54719977b3de7740c23b318bf6d3c
Paul Chavard <pro@paul.chavard.net>
2026-05-04 16:50:49 +0200

    feat(dossier): show updated by in champ badges

--- app/components/dossiers/champ_updated_component.rb
+++ app/components/dossiers/champ_updated_component.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class Dossiers::ChampUpdatedComponent < ApplicationComponent
-  attr_reader :updated_at
-  attr_reader :seen_at
+  attr_reader :updated_at, :seen_at, :updated_by, :source_stream
 
-  def initialize(updated_at: nil, seen_at: nil)
+  def initialize(updated_at: nil, seen_at: nil, updated_by: nil, source_stream: nil)
     @updated_at = updated_at
     @seen_at = seen_at
+    @updated_by = updated_by
+    @source_stream = source_stream
   end
 
   def badge_updated_class
@@ -14,6 +15,26 @@ class Dossiers::ChampUpdatedComponent < ApplicationComponent
       "fr-badge fr-badge--sm" => true,
       "fr-badge--new" => seen_at.present? && updated_at&.>(seen_at)
     )
+  end
+
+  def source
+    if instructeur?
+      t('.instructeur')
+    else
+      t('.usager')
+    end
+  end
+
+  def updated_by_instructeur?
+    updated_by.present? && instructeur?
+  end
+
+  def instructeur?
+    source_stream == Champ::INSTRUCTEUR_BUFFER_STREAM
+  end
+
+  def tooltip_id
+    @tooltip_id ||= "tooltip-#{SecureRandom.uuid}"
   end
 
   def render?
