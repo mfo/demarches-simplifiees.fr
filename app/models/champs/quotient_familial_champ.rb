9f69ac263d7823bfb1ec630d870782da7d5e01eb
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2026-05-06 16:17:14 +0200

    feat(api part) : add button action for user to refresh data

--- app/models/champs/quotient_familial_champ.rb
+++ app/models/champs/quotient_familial_champ.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Champs::QuotientFamilialChamp < Champ
+  REFRESH_DELAY = 24.hours
+
   attr_accessor :preview_state
 
   def fc_data_approved? = ActiveModel::Type::Boolean.new.cast(value)
