39781f21cffcabfab5d71ea9bd1fb8667adb82b9
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2026-05-11 14:56:23 +0200

    fix(quotient familial): do not reset external data when the user destroys an attachment

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -23,7 +23,7 @@ class AttachmentsController < ApplicationController
     if champ?
       @attachment = champ.piece_justificative_file.find { _1.blob.id == @blob.id }
       if @attachment.present?
-        champ.reset_external_data!
+        champ.reset_external_data! if !champ.france_connect?
 
         @attachment.purge_later
         champ.update_timestamps
