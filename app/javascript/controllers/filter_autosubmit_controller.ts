f62136ac5749bd0a0e5b5c31822742b04b4d1857
Kara Diaby <kdiaby.pro@gmail.com>
2026-05-20 18:38:20 +0200

    refactor(mes-dossiers): reuse existing autosubmit controller for filter panel

--- app/javascript/controllers/filter_autosubmit_controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@hotwired/stimulus';
-
-export default class FilterAutosubmitController extends Controller<HTMLFormElement> {
-  submit() {
-    this.element.requestSubmit();
-  }
-}
