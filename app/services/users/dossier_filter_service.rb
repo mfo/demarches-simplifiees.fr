demarche-numerique/demarche.numerique.gouv.fr

feat(mes-dossiers): add total_count and active? to DossierFilterService

app/services/users/dossier_filter_service.rb
@@ -26,14 +26,53 @@ module Users
     end
 
     def dossiers
-      scope = base_scope
-      scope = scope.joins(:procedure).where(procedures: { id: @params[:procedure_id] }) if @params[:procedure_id].present?
-      scope = scope.where(id: @user.dossiers_invites.visible_by_user) if shared_with_me?
-      scope = scope.where(state: model_states) if model_states.any?
-      scope = scope.where(id: alert_ids) if alert_scopes.any?
-      scope = scope.where('dossiers.created_at >= ?', from_created_at_date) if from_created_at_date
-      scope = scope.where('dossiers.depose_at >= ?', from_depose_at_date) if from_depose_at_date
-      scope.order(updated_at: :desc)
+      scope_without(:none).order(updated_at: :desc)
+    end
+
+    def total_count
+      dossiers.count
+    end
+
+    def counts
+      {
+        procedure_ids:  count_procedure_ids,
+        states:         count_states,
+        alerts:         count_alerts,
+        shared_with_me: scope_without(:shared_with_me).where(id: @user.dossiers_invites.visible_by_user).count
+      }
+    end
+
+    def active_filter_tags
+      tags = []
+      if @params[:procedure_id].present?
+        procedure = Procedure.find_by(id: @params[:procedure_id])
+        tags << { group: :procedure_id, value: @params[:procedure_id].to_s, label: procedure&.libelle.to_s }
+      end
+      if shared_with_me?
+        tags << { group: :shared_with_me, value: '1', label: I18n.t('dossiers.user_filter_panel_component.shared_with_me', default: 'Partagé avec moi') }
+      end
+      Array(@params[:state]).each do |s|
+        tags << { group: :state, value: s, label: I18n.t("dossiers.user_filter_panel_component.states.#{s}", default: s) }
+      end
+      Array(@params[:alert]).each do |a|
+        tags << { group: :alert, value: a, label: I18n.t("dossiers.user_filter_panel_component.alerts.#{a}", default: a) }
+      end
+      if from_created_at_date
+        tags << { group: :from_created_at_date, value: @params[:from_created_at_date], label: @params[:from_created_at_date].to_s }
+      end
+      if from_depose_at_date
+        tags << { group: :from_depose_at_date, value: @params[:from_depose_at_date], label: @params[:from_depose_at_date].to_s }
+      end
+      tags
+    end
+
+    def active?
+      @params[:procedure_id].present? ||
+        shared_with_me? ||
+        Array(@params[:state]).any? ||
+        Array(@params[:alert]).any? ||
+        from_created_at_date.present? ||
+        from_depose_at_date.present?
     end
 
     private
@@ -73,6 +112,35 @@ module Users
       Date.parse(raw)
     rescue Date::Error
       nil
+    end
+
+    def count_procedure_ids
+      scope_without(:procedure_id).joins(:procedure).group('procedures.id').count
+    end
+
+    def count_states
+      scope = scope_without(:state)
+      Users::DossierStateMapping::UI_STATES.index_with do |ui_state|
+        scope.where(state: Users::DossierStateMapping.model_state_for(ui_state)).count
+      end
+    end
+
+    def count_alerts
+      scope = scope_without(:alert)
+      ALERT_SCOPES.keys.index_with do |alert_key|
+        scope.where(id: Dossier.public_send(ALERT_SCOPES[alert_key]).pluck(:id)).count
+      end
+    end
+
+    def scope_without(group)
+      scope = base_scope
+      scope = scope.joins(:procedure).where(procedures: { id: @params[:procedure_id] }) if @params[:procedure_id].present? && group != :procedure_id
+      scope = scope.where(id: @user.dossiers_invites.visible_by_user) if shared_with_me? && group != :shared_with_me
+      scope = scope.where(state: model_states) if model_states.any? && group != :state
+      scope = scope.where(id: alert_ids) if alert_scopes.any? && group != :alert
+      scope = scope.where('dossiers.created_at >= ?', from_created_at_date) if from_created_at_date && group != :from_created_at_date
+      scope = scope.where('dossiers.depose_at >= ?', from_depose_at_date) if from_depose_at_date && group != :from_depose_at_date
+      scope
     end
   end
 end

spec/services/users/dossier_filter_service_spec.rb
@@ -115,6 +115,85 @@ RSpec.describe Users::DossierFilterService do
     end
   end
 
+  describe '#total_count' do
+    it 'returns the number of filtered dossiers' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['brouillon']))
+      expect(service.total_count).to eq(service.dossiers.count)
+    end
+  end
+
+  describe '#active?' do
+    it 'is false when no filter is applied' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new)
+      expect(service.active?).to be(false)
+    end
+
+    it 'ignores search as an active filter' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(search: 'foo'))
+      expect(service.active?).to be(false)
+    end
+
+    it 'is true when a filter other than search is applied' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['depose']))
+      expect(service.active?).to be(true)
+    end
+  end
+
+  describe '#counts' do
+    let!(:dossier_depose_corriger) { create(:dossier, :en_construction, user: user) }
+    let!(:dossier_depose_nothing) { create(:dossier, :en_construction, user: user) }
+    let!(:dossier_accepte) { create(:dossier, :accepte, user: user) }
+
+    before do
+      create(:dossier_correction, dossier: dossier_depose_corriger)
+    end
+
+    it 'returns counts for states (ignoring state filter itself)' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['depose']))
+      expect(service.counts[:states]['depose']).to eq(4)
+      expect(service.counts[:states]['accepte']).to eq(1)
+    end
+
+    it 'returns counts for alerts (contextualized by state filter)' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['depose']))
+      expect(service.counts[:alerts]['a_corriger']).to eq(1)
+    end
+
+    it 'returns counts grouped by procedure_id' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new)
+      expect(service.counts[:procedure_ids]).to be_a(Hash)
+    end
+
+    it 'returns shared_with_me count' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new)
+      expect(service.counts[:shared_with_me]).to eq(1)
+    end
+  end
+
+  describe '#active_filter_tags' do
+    let(:procedure) { create(:procedure, libelle: 'Demande de subvention') }
+
+    it 'returns one tag per active filter value' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(
+        procedure_id: procedure.id.to_s,
+        state: ['depose', 'en_instruction'],
+        alert: ['a_corriger']
+      ))
+      keys = service.active_filter_tags.map { |t| [t[:group], t[:value]] }
+      expect(keys).to contain_exactly(
+        [:procedure_id, procedure.id.to_s],
+        [:state, 'depose'],
+        [:state, 'en_instruction'],
+        [:alert, 'a_corriger']
+      )
+    end
+
+    it 'returns an empty list when no filter is active' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new)
+      expect(service.active_filter_tags).to eq([])
+    end
+  end
+
   describe '#dossiers with alert filter' do
     let!(:dossier_pending_correction) { create(:dossier, :en_construction, user: user) }
     let!(:dossier_pending_response) { create(:dossier, :en_construction, user: user) }

mfo/demarches-simplifiees.fr

app/services/users/dossier_filter_service.rb
@@ -8,7 +8,7 @@ module Users
       'nouveau_message'                 => :with_unread_messages_for_user,
       'message_avec_attente_de_reponse' => :with_pending_responses,
       'a_corriger'                      => :with_pending_corrections,
-      'expire_bientot'                  => :close_to_expiration
+      'expire_bientot'                  => :close_to_expiration,
     }.freeze
 
     def initialize(user:, params:)
@@ -26,14 +26,52 @@ module Users
     end
 
     def dossiers
-      scope = base_scope
-      scope = scope.joins(:procedure).where(procedures: { id: @params[:procedure_id] }) if @params[:procedure_id].present?
-      scope = scope.where(id: @user.dossiers_invites.visible_by_user) if shared_with_me?
-      scope = scope.where(state: model_states) if model_states.any?
-      scope = scope.where(id: alert_ids) if alert_scopes.any?
-      scope = scope.where('dossiers.created_at >= ?', from_created_at_date) if from_created_at_date
-      scope = scope.where('dossiers.depose_at >= ?', from_depose_at_date) if from_depose_at_date
-      scope.order(updated_at: :desc)
+      scope_without(:none).order(updated_at: :desc)
+    end
+
+    def total_count
+      dossiers.count
+    end
+
+    def counts
+      {
+        states:         count_states,
+        alerts:         count_alerts,
+        shared_with_me: scope_without(:shared_with_me).where(id: @user.dossiers_invites.visible_by_user).count,
+      }
+    end
+
+    def active_filter_tags
+      tags = []
+      if @params[:procedure_id].present?
+        procedure = Procedure.find_by(id: @params[:procedure_id])
+        tags << { group: :procedure_id, value: @params[:procedure_id].to_s, label: procedure&.libelle.to_s }
+      end
+      if shared_with_me?
+        tags << { group: :shared_with_me, value: '1', label: I18n.t('dossiers.user_filter_panel_component.shared_with_me', default: 'Partagé avec moi') }
+      end
+      Array(@params[:state]).each do |s|
+        tags << { group: :state, value: s, label: I18n.t("dossiers.user_filter_panel_component.states.#{s}", default: s) }
+      end
+      Array(@params[:alert]).each do |a|
+        tags << { group: :alert, value: a, label: I18n.t("dossiers.user_filter_panel_component.alerts.#{a}", default: a) }
+      end
+      if from_created_at_date
+        tags << { group: :from_created_at_date, value: @params[:from_created_at_date], label: @params[:from_created_at_date].to_s }
+      end
+      if from_depose_at_date
+        tags << { group: :from_depose_at_date, value: @params[:from_depose_at_date], label: @params[:from_depose_at_date].to_s }
+      end
+      tags
+    end
+
+    def active?
+      @params[:procedure_id].present? ||
+        shared_with_me? ||
+        Array(@params[:state]).any? ||
+        Array(@params[:alert]).any? ||
+        from_created_at_date.present? ||
+        from_depose_at_date.present?
     end
 
     private
@@ -48,12 +86,10 @@ module Users
 
     def model_states
       Array(@params[:state])
-        .map { |s| Users::DossierStateMapping.model_state_for(s) }
-        .compact
     end
 
     def alert_scopes
-      Array(@params[:alert]).map { |a| ALERT_SCOPES[a] }.compact
+      Array(@params[:alert]).filter_map { |a| ALERT_SCOPES[a] }
     end
 
     def alert_ids
@@ -73,6 +109,31 @@ module Users
       Date.parse(raw)
     rescue Date::Error
       nil
+    end
+
+    def count_states
+      scope = scope_without(:state)
+      Users::DossierStateMapping::UI_STATES.index_with do |state|
+        scope.where(state: state).count
+      end
+    end
+
+    def count_alerts
+      scope = scope_without(:alert)
+      ALERT_SCOPES.keys.index_with do |alert_key|
+        scope.where(id: Dossier.public_send(ALERT_SCOPES[alert_key]).select(:id)).count
+      end
+    end
+
+    def scope_without(group)
+      scope = base_scope
+      scope = scope.joins(:procedure).where(procedures: { id: @params[:procedure_id] }) if @params[:procedure_id].present? && group != :procedure_id
+      scope = scope.where(id: @user.dossiers_invites.visible_by_user) if shared_with_me? && group != :shared_with_me
+      scope = scope.where(state: model_states) if model_states.any? && group != :state
+      scope = scope.where(id: alert_ids) if alert_scopes.any? && group != :alert
+      scope = scope.where(dossiers: { created_at: from_created_at_date.. }) if from_created_at_date && group != :from_created_at_date
+      scope = scope.where(dossiers: { depose_at: from_depose_at_date.. }) if from_depose_at_date && group != :from_depose_at_date
+      scope
     end
   end
 end

spec/services/users/dossier_filter_service_spec.rb
@@ -73,20 +73,15 @@ RSpec.describe Users::DossierFilterService do
     let!(:accepte) { create(:dossier, :accepte, user: user) }
 
     it 'filters by a single UI state' do
-      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['depose']))
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['en_construction']))
       expect(service.dossiers).to include(en_construction)
       expect(service.dossiers).not_to include(brouillon, en_instruction, accepte)
     end
 
     it 'filters by multiple states (OR)' do
-      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['brouillon', 'depose']))
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['brouillon', 'en_construction']))
       expect(service.dossiers).to include(brouillon, en_construction)
       expect(service.dossiers).not_to include(en_instruction, accepte)
-    end
-
-    it 'ignores unknown state values' do
-      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['unknown']))
-      expect(service.dossiers).to include(own_dossier, invited_dossier, brouillon, en_construction, en_instruction, accepte)
     end
   end
 
@@ -112,6 +107,80 @@ RSpec.describe Users::DossierFilterService do
       service = described_class.new(user: user, params: ActionController::Parameters.new(from_created_at_date: 'not-a-date'))
       expect { service.dossiers.to_a }.not_to raise_error
       expect(service.dossiers).to include(old_dossier, recent_dossier)
+    end
+  end
+
+  describe '#total_count' do
+    it 'returns the number of filtered dossiers' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['brouillon']))
+      expect(service.total_count).to eq(service.dossiers.count)
+    end
+  end
+
+  describe '#active?' do
+    it 'is false when no filter is applied' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new)
+      expect(service.active?).to be(false)
+    end
+
+    it 'ignores search as an active filter' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(search: 'foo'))
+      expect(service.active?).to be(false)
+    end
+
+    it 'is true when a filter other than search is applied' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['en_construction']))
+      expect(service.active?).to be(true)
+    end
+  end
+
+  describe '#counts' do
+    let!(:dossier_depose_corriger) { create(:dossier, :en_construction, user: user) }
+    let!(:dossier_depose_nothing) { create(:dossier, :en_construction, user: user) }
+    let!(:dossier_accepte) { create(:dossier, :accepte, user: user) }
+
+    before do
+      create(:dossier_correction, dossier: dossier_depose_corriger)
+    end
+
+    it 'returns counts for states (ignoring state filter itself)' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['en_construction']))
+      expect(service.counts[:states]['en_construction']).to eq(4)
+      expect(service.counts[:states]['accepte']).to eq(1)
+    end
+
+    it 'returns counts for alerts (contextualized by state filter)' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(state: ['en_construction']))
+      expect(service.counts[:alerts]['a_corriger']).to eq(1)
+    end
+
+    it 'returns shared_with_me count' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new)
+      expect(service.counts[:shared_with_me]).to eq(1)
+    end
+  end
+
+  describe '#active_filter_tags' do
+    let(:procedure) { create(:procedure, libelle: 'Demande de subvention') }
+
+    it 'returns one tag per active filter value' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new(
+        procedure_id: procedure.id.to_s,
+        state: ['en_construction', 'en_instruction'],
+        alert: ['a_corriger']
+      ))
+      keys = service.active_filter_tags.map { |t| [t[:group], t[:value]] }
+      expect(keys).to contain_exactly(
+        [:procedure_id, procedure.id.to_s],
+        [:state, 'en_construction'],
+        [:state, 'en_instruction'],
+        [:alert, 'a_corriger']
+      )
+    end
+
+    it 'returns an empty list when no filter is active' do
+      service = described_class.new(user: user, params: ActionController::Parameters.new)
+      expect(service.active_filter_tags).to eq([])
     end
   end
 