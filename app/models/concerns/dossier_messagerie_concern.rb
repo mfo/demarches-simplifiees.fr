demarche-numerique/demarche.numerique.gouv.fr

Add Dossier#unread_message_for_user? predicate for new message notification
Introduces DossierMessagerieConcern with an unread_commentaires_for_user
association and the unread_message_for_user? predicate, so usagers can be
notified of unseen agent messages that aren't already covered by the
higher-priority correction/pending-response notifications.

app/models/concerns/dossier_messagerie_concern.rb
@@ -1,0 +1,24 @@
+# frozen_string_literal: true
+
+module DossierMessagerieConcern
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :unread_commentaires_for_user,
+      -> { sent_by_agent.unread_by_recipient },
+      class_name: 'Commentaire',
+      inverse_of: :dossier
+  end
+
+  # Un message « nouveau message » est un message d'agent non lu par l'usager.
+  # Il est volontairement en retrait derrière les deux notifications prioritaires
+  # « à corriger » et « en attente de réponse » (qui sont aussi des messages d'agent
+  # non lus) pour n'afficher qu'un seul badge de type messagerie à la fois.
+  def unread_message_for_user?
+    return false if pending_correction? || pending_response?
+
+    # `.any?` lit l'association préchargée en mémoire (aucune requête dans la
+    # liste, cf. USER_LIST_PRELOADS), et retombe sur un `exists?` léger sinon.
+    unread_commentaires_for_user.any?
+  end
+end

spec/models/concerns/dossier_messagerie_concern_spec.rb
@@ -1,0 +1,83 @@
+# frozen_string_literal: true
+
+RSpec.describe DossierMessagerieConcern do
+  describe '#unread_message_for_user?' do
+    let(:dossier) { create(:dossier, :en_construction) }
+
+    subject { dossier.unread_message_for_user? }
+
+    context 'when an instructeur sent an unread message' do
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when an expert sent an unread message' do
+      before { create(:commentaire, dossier:, expert: create(:expert), seen_by_recipient_at: nil) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when the dossier is en_instruction' do
+      let(:dossier) { create(:dossier, :en_instruction) }
+
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when the agent message has been seen' do
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: 1.day.ago) }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the agent message is discarded' do
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil, discarded_at: Time.current) }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the message was sent by the usager' do
+      before { create(:commentaire, dossier:, seen_by_recipient_at: nil) }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the dossier has no message' do
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the dossier is pending_correction' do
+      before do
+        create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil)
+        create(:dossier_correction, dossier:)
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the dossier is pending_response' do
+      before do
+        create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil)
+        create(:dossier_pending_response, dossier:)
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the association is preloaded' do
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil) }
+
+      it 'reads the loaded association' do
+        preloaded = Dossier
+          .where(id: dossier.id)
+          .includes(:unread_commentaires_for_user, :pending_corrections, :awaiting_responses)
+          .first
+
+        expect(preloaded.unread_commentaires_for_user).to be_loaded
+        expect(preloaded.unread_message_for_user?).to be_truthy
+      end
+    end
+  end
+end

mfo/demarches-simplifiees.fr

app/models/concerns/dossier_messagerie_concern.rb
@@ -1,0 +1,24 @@
+# frozen_string_literal: true
+
+module DossierMessagerieConcern
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :unread_messages_for_user,
+      -> { sent_by_agent.unread_by_recipient },
+      class_name: 'Commentaire',
+      inverse_of: :dossier
+  end
+
+  # Un message « nouveau message » est un message d'agent non lu par l'usager.
+  # Il est volontairement en retrait derrière les deux notifications prioritaires
+  # « à corriger » et « en attente de réponse » (qui sont aussi des messages d'agent
+  # non lus) pour n'afficher qu'un seul badge de type messagerie à la fois.
+  def unread_message_for_user?
+    return false if pending_correction? || pending_response?
+
+    # `.any?` lit l'association préchargée en mémoire (aucune requête dans la
+    # liste, cf. USER_LIST_PRELOADS), et retombe sur un `exists?` léger sinon.
+    unread_messages_for_user.any?
+  end
+end

spec/models/concerns/dossier_messagerie_concern_spec.rb
@@ -1,0 +1,83 @@
+# frozen_string_literal: true
+
+RSpec.describe DossierMessagerieConcern do
+  describe '#unread_message_for_user?' do
+    let(:dossier) { create(:dossier, :en_construction) }
+
+    subject { dossier.unread_message_for_user? }
+
+    context 'when an instructeur sent an unread message' do
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when an expert sent an unread message' do
+      before { create(:commentaire, dossier:, expert: create(:expert), seen_by_recipient_at: nil) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when the dossier is en_instruction' do
+      let(:dossier) { create(:dossier, :en_instruction) }
+
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when the agent message has been seen' do
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: 1.day.ago) }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the agent message is discarded' do
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil, discarded_at: Time.current) }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the message was sent by the usager' do
+      before { create(:commentaire, dossier:, seen_by_recipient_at: nil) }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the dossier has no message' do
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the dossier is pending_correction' do
+      before do
+        create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil)
+        create(:dossier_correction, dossier:)
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the dossier is pending_response' do
+      before do
+        create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil)
+        create(:dossier_pending_response, dossier:)
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the association is preloaded' do
+      before { create(:commentaire, dossier:, instructeur: create(:instructeur), seen_by_recipient_at: nil) }
+
+      it 'reads the loaded association' do
+        preloaded = Dossier
+          .where(id: dossier.id)
+          .includes(:unread_messages_for_user, :pending_corrections, :awaiting_responses)
+          .first
+
+        expect(preloaded.unread_messages_for_user).to be_loaded
+        expect(preloaded.unread_message_for_user?).to be_truthy
+      end
+    end
+  end
+end