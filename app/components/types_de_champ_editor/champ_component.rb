a18f41545232b78359a6611039eb198447862c8b
simon lehericey <mail@simon.lehericey.net>
2026-08-18 17:20:11 +0200

    tech: iterate over the type de champ classes in the editor
    The type select filtered enum values and asked find_sti_class in every
    filter; map to the classes once, filter on them and read sti_name back when
    rendering the options.

--- app/components/types_de_champ_editor/champ_component.rb
+++ app/components/types_de_champ_editor/champ_component.rb
@@ -60,16 +60,17 @@ class TypesDeChampEditor::ChampComponent < ApplicationComponent
     cat_scope = "activerecord.attributes.type_de_champ.categorie"
     tdc_scope = "activerecord.attributes.type_de_champ.type_champs"
     TypeDeChamp.type_champs.keys
+      .map { TypeDeChamp.find_sti_class(_1) }
       .filter(&method(:filter_type_champ))
       .filter(&method(:filter_featured_type_champ))
       .filter(&method(:filter_block_type_champ))
       .filter(&method(:filter_public_or_private_only_type_champ))
-      .group_by { TypeDeChamp.category_for(_1) }
+      .group_by(&:category)
       .sort_by { |k, _v| TypeDeChamp::CATEGORIES.find_index(k) }
-      .to_h do |cat, tdc|
+      .to_h do |cat, klasses|
         [
           t(cat, scope: cat_scope),
-          tdc.map { [t(_1, scope: tdc_scope), _1, { disabled: !accepted_type_champs.include?(_1) }] },
+          klasses.map { [t(_1.sti_name, scope: tdc_scope), _1.sti_name, { disabled: !accepted_type_champs.include?(_1.sti_name) }] },
         ]
       end
   end
@@ -102,27 +103,20 @@ class TypesDeChampEditor::ChampComponent < ApplicationComponent
     }
   end
 
-  def filter_block_type_champ(type_champ)
-    !coordinate.child? || TypeDeChamp.find_sti_class(type_champ).allowed_in_repetition?
+  def filter_block_type_champ(klass)
+    !coordinate.child? || klass.allowed_in_repetition?
   end
 
-  def filter_public_or_private_only_type_champ(type_champ)
-    klass = TypeDeChamp.find_sti_class(type_champ)
+  def filter_public_or_private_only_type_champ(klass)
     coordinate.private? ? !klass.public_only? : !klass.private_only?
   end
 
-  def filter_featured_type_champ(type_champ)
-    feature_name = TypeDeChamp.find_sti_class(type_champ).feature_flag
-    feature_name.nil? || procedure.feature_enabled?(feature_name)
+  def filter_featured_type_champ(klass)
+    klass.feature_flag.nil? || procedure.feature_enabled?(klass.feature_flag)
   end
 
-  def filter_type_champ(type_champ)
-    case type_champ
-    when TypeDeChamp.type_champs.fetch(:number)
-      has_legacy_number?
-    else
-      true
-    end
+  def filter_type_champ(klass)
+    klass != TypesDeChamp::NumberTypeDeChamp || has_legacy_number?
   end
 
   def has_legacy_number?
