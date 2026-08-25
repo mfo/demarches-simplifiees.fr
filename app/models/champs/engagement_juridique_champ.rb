6dbaf04b208a8cbb84c9a7714962bb3510698d5a
simon lehericey <mail@simon.lehericey.net>
2026-08-25 13:58:18 +0200

    fix(champs): anchor the engagement juridique number format

--- app/models/champs/engagement_juridique_champ.rb
+++ app/models/champs/engagement_juridique_champ.rb
@@ -4,7 +4,7 @@ class Champs::EngagementJuridiqueChamp < ChampData
   # cf: https://communaute.chorus-pro.gouv.fr/documentation/creer-un-engagement/#1522314752186-a34f3662-0644b5d1-16c22add-8ea097de-3a0a
   validates :value,
             format: {
-              with: /([A-Z]|[0-9]|\-|\_|\+|\/)+/,
+              with: /\A[A-Z0-9_+\/-]+\z/,
               message: "Le numéro d'EJ ne peut contenir que des caractères alphanumérique et les caractères spéciaux suivant : “-“ ; “_“ ; “+“ ; “/“",
             },
             allow_blank: true,
