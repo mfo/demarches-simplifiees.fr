b809646fa5c4baeb03d0c69cc924e354a781d8d2
simon lehericey <mail@simon.lehericey.net>
2026-08-25 11:32:33 +0200

    tech: let the pre rempli own its drop-down options

--- app/models/types_de_champ/pre_rempli_type_de_champ.rb
+++ app/models/types_de_champ/pre_rempli_type_de_champ.rb
@@ -7,14 +7,22 @@ class TypesDeChamp::PreRempliTypeDeChamp < TypeDeChamp
   def self.column_type = :enum
   def self.conditionable? = true
 
+  store_accessor :options, :drop_down_options, :pre_rempli_hidden
+  boolean_options :pre_rempli_hidden
+
   def condition_value_type = :enum
-  def condition_options = options_for_select_with_other
-
-  include TypesDeChamp::DropDownOptionsConcern
-
+  def condition_options = options_for_select
   def prefillable? = true
   def options_for_select = drop_down_options.uniq.map { [_1, _1] }
   def cannot_be_mandatory? = true
-  store_accessor :options, :pre_rempli_hidden
-  boolean_options :pre_rempli_hidden
+
+  def drop_down_options = Array.wrap(super)
+
+  def drop_down_options=(options)
+    super(Array.wrap(options).filter_map { it.to_s.squish.presence })
+  end
+
+  def drop_down_options_from_text=(text)
+    self.drop_down_options = text.to_s.lines
+  end
 end
