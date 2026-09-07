demarche-numerique/demarche.numerique.gouv.fr

ETQ usager ProConnecté, mon SIRET est prérempli à l'étape établissement
Un usager ProConnecté qui commence une démarche personne morale ressaisissait
le SIRET que ProConnect vient pourtant de transmettre.

Le champ est prérempli et accompagné d'un bandeau « transmis par ProConnect »,
mais il reste modifiable : une organisation a plusieurs établissements et un
mandataire peut légitimement déposer pour un autre SIRET.

Le SIRET mémorisé sur l'usager (dossier précédent) reste prioritaire.

Ref #13327

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

spec/models/identity_prefill_source_spec.rb
@@ -114,4 +114,43 @@ RSpec.describe IdentityPrefillSource do
       it { expect(source.mandataire_locked?).to be(false) }
     end
   end
+
+  describe "#pro_connect_siret" do
+    subject { source.pro_connect_siret }
+
+    context "when the session is ProConnected" do
+      let(:user) { create(:user, :with_pci) }
+      let(:pro_connect) { true }
+
+      it { is_expected.to eq('12345678901234') }
+
+      context "and ProConnect did not provide an identity" do
+        let(:user) { create(:user, pro_connect_informations: [build(:pro_connect_information, given_name: nil, usual_name: nil)]) }
+
+        it "is still available: the siret does not depend on the identity being complete" do
+          expect(source.name).to be_nil
+          expect(subject).to eq('12345678901234')
+        end
+      end
+
+      context "and ProConnect did not provide a siret" do
+        let(:user) { create(:user, pro_connect_informations: [build(:pro_connect_information, siret: nil)]) }
+
+        it { is_expected.to be_nil }
+      end
+
+      context "but the user has no ProConnect information" do
+        let(:user) { create(:user) }
+
+        it { is_expected.to be_nil }
+      end
+    end
+
+    context "when the session is not ProConnected" do
+      let(:user) { create(:user, :with_pci) }
+      let(:pro_connect) { false }
+
+      it { is_expected.to be_nil }
+    end
+  end
 end

mfo/demarches-simplifiees.fr

spec/models/identity_prefill_source_spec.rb
@@ -112,4 +112,42 @@ RSpec.describe IdentityPrefillSource do
       it { expect(identity_source.mandataire_locked?).to be(false) }
     end
   end
+
+  describe "#pro_connect_siret" do
+    subject { identity_source.pro_connect_siret }
+
+    context "when the session is ProConnected" do
+      let(:user) { create(:user, :with_pci) }
+      let(:source) { :pro_connect }
+
+      it { is_expected.to eq('12345678901234') }
+
+      context "and ProConnect did not provide an identity" do
+        let(:user) { create(:user, pro_connect_informations: [build(:pro_connect_information, given_name: nil, usual_name: nil)]) }
+
+        it "is still available: the siret does not depend on the identity being complete" do
+          expect(identity_source.resolved).to be_nil
+          expect(subject).to eq('12345678901234')
+        end
+      end
+
+      context "and ProConnect did not provide a siret" do
+        let(:user) { create(:user, pro_connect_informations: [build(:pro_connect_information, siret: nil)]) }
+
+        it { is_expected.to be_nil }
+      end
+
+      context "but the user has no ProConnect information" do
+        let(:user) { create(:user) }
+
+        it { is_expected.to be_nil }
+      end
+    end
+
+    context "when the session is not ProConnected" do
+      let(:user) { create(:user, :with_pci) }
+
+      it { is_expected.to be_nil }
+    end
+  end
 end